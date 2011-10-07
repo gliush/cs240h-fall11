e8ad90ec2a5f86b452a551a41a6158e0819bca44
Ivan Glushkov <gli.work@gmail.com>
2011-10-08 01:35:29 +0400

    hlint reports detected

--- labs/lab1/task.hs
+++ labs/lab1/task.hs
@@ -6,31 +6,30 @@ import Data.Map(empty,toList,alter,Map)
 main = do
     lines <- getContents
     let strings = analyze lines
-    mapM putStrLn strings
-    return ()
+    mapM_ putStrLn strings
 
 analyze :: String -> [String]
 analyze lines = strings 
     where
-        allwords = words $ map toLower $ rm_punct lines
+        allwords = words $ map toLower $ rmPunct lines
         wordsFreq = freq allwords
         wordsFreqNorm = normalize wordsFreq
         strings = map stringRepr wordsFreqNorm
 
-rm_punct :: String -> String
-rm_punct [] = []
-rm_punct (x:xs) | is_punct(x) = ' ':rm_punct(xs)
-                | otherwise = x:rm_punct(xs)
+rmPunct :: String -> String
+rmPunct [] = []
+rmPunct (x:xs) | isPunct x = ' ':rmPunct xs
+                | otherwise = x:rmPunct xs
 
-is_punct x = elem x ",.:;-!@#$%^&*()+=|]}[{`~\"?/><"
+isPunct x = x `elem` ",.:;-!@#$%^&*()+=|]}[{`~\"?/><"
 
 freq :: [String] -> [(String, Int)]
-freq allwords = sortit  $ freq_ll allwords empty
+freq allwords = sortit  $ freq' allwords empty
     where sortit = sortBy (\(_, a) (_, b) -> compare b a)
 
-freq_ll :: [String] -> Map String Int -> [(String, Int)]
-freq_ll [] acc = toList acc
-freq_ll (word:words) acc = freq_ll words newacc
+freq' :: [String] -> Map String Int -> [(String, Int)]
+freq' [] acc = toList acc
+freq' (word:words) acc = freq' words newacc
     where
         f (Just v) = Just (v+1)
         f Nothing = Just 1
@@ -41,10 +40,10 @@ normalize input =
     [(word, maxWord, n) | (word,c) <- input, let n = number c, n > 0 ] 
     where
         maxWord = maximum $ map (length.fst) input
-        maxCount = (maximum $ map snd input)
-        mult = (fromIntegral (79 - maxWord)) / (fromIntegral maxCount)
-        number c = truncate ((fromIntegral c) * mult)
+        maxCount = maximum $ map snd input
+        mult = fromIntegral (79 - maxWord) / fromIntegral maxCount
+        number c = truncate (fromIntegral c * mult)
   
 stringRepr :: (String, Int, Int) -> String
 stringRepr (word, maxLen, num) = 
-    word ++ replicate (1+maxLen-length(word)) ' ' ++ replicate num '#'
+    word ++ replicate (1+maxLen-length word) ' ' ++ replicate num '#'
